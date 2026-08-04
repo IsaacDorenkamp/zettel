c801fcc95a6535d1815097eb5e86d7022d80e179
Isaac Dorenkamp <idorenkamp@csgactuarial.com>
2026-08-04 16:27:50 -0500

    feat(wip): re-implement Id

--- include/ident.hpp
+++ include/ident.hpp
@@ -21,84 +21,17 @@ private:
 
 class Id {
 public:
+    enum class Type {
+        Numeric = 0, Classic = 1
+    };
+    using Representation = std::variant<uint32_t, std::string>;
+    Id(Representation repr);
     virtual ~Id() = default;
 
-    virtual Buffer encode() const = 0;
-    virtual std::string toString() const = 0;
-    virtual bool operator==(const Id& other) const = 0;
-    virtual std::unique_ptr<Id> clone() const = 0;
-    virtual uint32_t hash() const = 0;
-
-    std::ostream& write(std::ostream& stream) const;
-
-    friend std::ostream& operator<<(std::ostream& os, const Id& id);
-};
-
-class NumericalId : public Id {
-public:
-    NumericalId(uint32_t id);
-
-    uint32_t id() const;
     uint32_t hash() const;
-
-    virtual Buffer encode() const;
-    virtual std::string toString() const;
-    virtual bool operator==(const Id& other) const;
-    virtual std::unique_ptr<Id> clone() const;
+    bool operator==(const Id& other) const;
 private:
-    uint32_t m_id;
-    std::string m_repr;
-
-    void construct_repr();
-};
-
-class IdGenerator {
-public:
-    virtual ~IdGenerator() = default;
-    virtual std::unique_ptr<Id> next() = 0;
-    virtual void consume(const Id& used) = 0;
-};
-
-template <class IdType>
-class TypedIdGenerator : public IdGenerator {
-public:
-    TypedIdGenerator() = default;
-    virtual ~TypedIdGenerator() = default;
-
-    virtual std::unique_ptr<Id> next() {
-        return std::unique_ptr<Id>(static_cast<Id*>(typedNext().release()));
-    }
-    virtual void consume(const Id& used) {
-        const Id* ptr = &used;
-        if (const IdType* casted = dynamic_cast<const IdType*>(ptr)) {
-            typedConsume(*casted);
-        }
-        // we can ignore any ID of an unmatched type
-    }
-
-    virtual std::unique_ptr<IdType> typedNext() = 0;
-    virtual void typedConsume(const IdType& used) = 0;
-};
-
-class NumericalIdGenerator : public TypedIdGenerator<NumericalId> {
-public:
-    NumericalIdGenerator();
-    virtual ~NumericalIdGenerator() = default;
-
-    virtual std::unique_ptr<NumericalId> typedNext();
-    virtual void typedConsume(const NumericalId& used);
-private:
-    uint32_t m_next;
-};
-
-
-template <typename T>
-class IdParser;
-
-template <>
-class IdParser<NumericalId> {
-public:
-    static std::unique_ptr<NumericalId> parse(std::string source);
+    Representation m_value;
 };
 
 }
